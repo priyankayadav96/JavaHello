960d23206559ce03098d730a26606dd39b99b567
Priyanka Yadav <priyanka.yadav@accoliteindia.com>
2017-07-10 22:13:13 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello World");
+          int i;
+          for(i=0;i<10;i++)
+          {	  
+        	  i=i+2;
+        	  System.out.println(i);
+          }
+	}
+
+}
